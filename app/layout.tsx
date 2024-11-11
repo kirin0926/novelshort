f80b7d0fa352595d51be7e71ecf3f62138099937
novelshort <yq963657293@outlook.com>
2024-11-11 21:06:52 +0800

    新增谷歌ad广告

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7897104007345492"
+     crossorigin="anonymous"></script> 
       <head />
       <body>
         <ClientProviders>
